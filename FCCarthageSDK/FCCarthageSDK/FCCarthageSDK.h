251e82e0fd8282f4830033dca16efba6bcbf4dfc
Harish Kumar <harishk@freshdesk.com>
2018-08-17 17:53:48 +0530

    Initial commit

---
+++ FCCarthageSDK/FCCarthageSDK/FCCarthageSDK.h
@@ -0,0 +1,20 @@
+//
+//  FCCarthageSDK.h
+//  FCCarthageSDK
+//
+//  Created by Harish Kumar on 17/08/18.
+//  Copyright © 2018 Freshdesk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FCCarthageSDK/FreshchatSDK.h>
+
+//! Project version number for FCCarthageSDK.
+FOUNDATION_EXPORT double FCCarthageSDKVersionNumber;
+
+//! Project version string for FCCarthageSDK.
+FOUNDATION_EXPORT const unsigned char FCCarthageSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FCCarthageSDK/PublicHeader.h>
+
+
